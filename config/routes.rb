f57db6e25cac6974c547aa374b8046122fde0af2
samuel <samiequipo@gmail.com>
2020-12-03 18:53:17 -0400

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  post 'contacts', to: "contacts#create"
   root 'pages#index'
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
