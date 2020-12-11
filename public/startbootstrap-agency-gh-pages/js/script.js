f67a75d353fe9513abe82d765be4178f68695824
samuel <samiequipo@gmail.com>
2020-12-10 22:40:12 -0400

    Table

--- public/startbootstrap-agency-gh-pages/js/script.js
+++ public/startbootstrap-agency-gh-pages/js/script.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["<span style='color:#fbd454'>Nos</span>", "<span style='color:#012e48'>Meo</span>", "<span style='color:#fb4d29'>Nowo</span>", "<span style='color:#e73321'>Vodafone</span>"],
+    strings: ["<span style='color:#fbd454'>Nos</span>", "<span style='color:#0fbcf9'>Meo</span>", "<span style='color:#fb4d29'>Nowo</span>", "<span style='color:#e73321'>Vodafone</span>"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
