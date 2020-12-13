0d27571e9704c60562bea997967dc2ad7a73187e
samuel <samiequipo@gmail.com>
2020-12-13 13:21:52 -0400

    experience card

--- public/startbootstrap-agency-gh-pages/css/js/script.js
+++ public/startbootstrap-agency-gh-pages/css/js/script.js
@@ -8,10 +8,4 @@ $(document).ready(function () {
     loop: true
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
 });
--- public/startbootstrap-agency-gh-pages/js/script.js
+++ public/startbootstrap-agency-gh-pages/js/script.js
@@ -43,32 +43,4 @@ $(document).ready(function () {
     loop: true
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-
-  // owl carousel script
-  $('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false
-      },
-      600: {
-        items: 2,
-        nav: false
-      },
-      1000: {
-        items: 3,
-        nav: false
-      }
-    }
-  });
 });
