ce6226d5736eb2b39d163aefd7822d63bb58c6ac
samuel <samiequipo@gmail.com>
2020-12-26 09:43:57 -0400

    Change typing order

--- public/startbootstrap-agency-gh-pages/js/script.js
+++ public/startbootstrap-agency-gh-pages/js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["<span style='color:#fbd454'>Nos</span>", "<span style='color:#0fbcf9'>Meo</span>", "<span style='color:#fb4d29'>Nowo</span>", "<span style='color:#e73321'>Vodafone</span>"],
+    strings: ["<span style='color:#0fbcf9'>Meo</span>",
+      "<span style='color:#fbd454'>Nos</span>",
+      "<span style='color:#e73321'>Vodafone</span>",
+      "<span style='color:#fb4d29'>Nowo</span>"
+    ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
