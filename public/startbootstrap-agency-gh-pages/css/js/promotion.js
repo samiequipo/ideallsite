0d0d3ad10ee8eed8522c6dcbaa86675e35a86b16
samuel <samiequipo@gmail.com>
2021-01-21 21:58:12 -0400

    remove files

--- public/startbootstrap-agency-gh-pages/css/js/promotion.js
+++
@@ -1,6 +0,0 @@
-function imgSlider(captureImage, color, letter) {
-  document.querySelector('.starbucks').src = captureImage;
-  document.querySelector('.circle').style.background = color;
-  document.querySelector('.letter').style.color = color;
-  document.querySelector('.btn--portfolio').style.background = color;
-}
