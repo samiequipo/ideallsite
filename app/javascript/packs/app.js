0dba52e13302dbbf39d1385b8cf8cf7d28bb4997
samuel <samiequipo@gmail.com>
2021-01-21 21:39:19 -0400

    fix page add app glider

---
+++ app/javascript/packs/app.js
@@ -0,0 +1,65 @@
+window.addEventListener('load', function () {
+	const glider = new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente',
+			loop: true
+		},
+		responsive: [{
+			// screens greater than >= 775px
+			breakpoint: 450,
+			settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				loop: true
+			}
+		}, {
+			// screens greater than >= 1024px
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				draggable: true,
+				scrollLock: true,
+				autoplay: 1,
+				animationDuration: 300000,
+				animationTimingFunc: 'linear',
+				rewind: true,
+				loop: true
+			}
+		}]
+
+	});
+
+	//
+	function sliderAuto(slider, miliseconds) {
+		const slidesCount = slider.track.childElementCount;
+		let slideTimeout = null;
+		let nextIndex = 1;
+
+		function slide() {
+			slideTimeout = setTimeout(
+				function () {
+					if (nextIndex >= slidesCount) {
+						nextIndex = 0;
+					}
+					slider.scrollItem(nextIndex++);
+				},
+				miliseconds
+			);
+		}
+
+		slider.ele.addEventListener('glider-animated', function () {
+			window.clearInterval(slideTimeout);
+			slide();
+		});
+
+		slide();
+	}
+
+	sliderAuto(glider, 400000)
+});
