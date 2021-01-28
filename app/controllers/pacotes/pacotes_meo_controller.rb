f33c12cb3a4d1b61186f1067d27fd042ce54a396
samuel <samiequipo@gmail.com>
2021-01-28 17:37:39 -0400

    fix routes

--- app/controllers/pacotes/pacotes_meo_controller.rb
+++ app/controllers/pacotes/pacotes_meo_controller.rb
@@ -1,18 +1,25 @@
 class Pacotes::PacotesMeoController < ApplicationController
+  def index
+  
+  end
+  
   #M4
   def telemovel
-    
+    respond_to do |format|
+      format.js { render partial: 'pacotes/pacotes_meo/telemovel' }
+    end   
   end
   
   #M3
   def tv_net_voz
-  
+    respond_to do |format|
+      format.js { render partial: 'example/nos_m4' }
+    end   
   end
   
   def gaming
   
   end
-
   def outros
   
   end
