3f4d8927fd1358fd07d1af9766df5febcb55fbd6
samuel <samiequipo@gmail.com>
2021-01-30 12:01:20 -0400

    refactorin pacotes meo

--- app/controllers/pacotes/pacotes_meo_controller.rb
+++ app/controllers/pacotes/pacotes_meo_controller.rb
@@ -1,31 +1,29 @@
 class Pacotes::PacotesMeoController < ApplicationController
+  before_action :render_pacotes, except: %i[index]
+  
   def index  
   end
   
   #M4
   def tv_net_voz
-    respond_to do |format|
-      format.js { render partial: 'pacotes/pacotes_meo/tv_net_voz' }
-    end 
   end
 
   #M3
   def telemovel
-    respond_to do |format|
-      format.js { render partial: 'pacotes/pacotes_meo/telemovel' }
-    end   
   end
   
   
   def gaming
-    respond_to do |format|
-      format.js { render partial: 'pacotes/pacotes_meo/gaming' }
-    end   
   end
   
   def outros
+  end
+
+  private
+
+  def render_pacotes
     respond_to do |format|
-      format.js { render partial: 'pacotes/pacotes_meo/outros' }
-    end
+      format.js { render partial: "pacotes/pacotes_meo/#{params[:option]}" }
+    end 
   end
 end
