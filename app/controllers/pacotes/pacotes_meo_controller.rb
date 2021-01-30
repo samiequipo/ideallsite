c4b7977ba03929c4f0b8e10aef78d6ca377ce5e8
samuel <samiequipo@gmail.com>
2021-01-29 23:22:09 -0400

    Add meo structure and fix page

--- app/controllers/pacotes/pacotes_meo_controller.rb
+++ app/controllers/pacotes/pacotes_meo_controller.rb
@@ -1,26 +1,31 @@
 class Pacotes::PacotesMeoController < ApplicationController
-  def index
-  
+  def index  
   end
   
   #M4
+  def tv_net_voz
+    respond_to do |format|
+      format.js { render partial: 'pacotes/pacotes_meo/tv_net_voz' }
+    end 
+  end
+
+  #M3
   def telemovel
     respond_to do |format|
       format.js { render partial: 'pacotes/pacotes_meo/telemovel' }
     end   
   end
   
-  #M3
-  def tv_net_voz
+  
+  def gaming
     respond_to do |format|
-      format.js { render partial: 'example/nos_m4' }
+      format.js { render partial: 'pacotes/pacotes_meo/gaming' }
     end   
   end
   
-  def gaming
-  
-  end
   def outros
-  
+    respond_to do |format|
+      format.js { render partial: 'pacotes/pacotes_meo/outros' }
+    end
   end
 end
