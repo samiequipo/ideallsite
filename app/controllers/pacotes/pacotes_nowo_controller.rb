4f0b345b354c01d7a4151b79b7cb4ca968827ccf
samuel <samiequipo@gmail.com>
2021-01-31 13:22:44 -0400

    Fixing pacotes meo and its navbar

--- app/controllers/pacotes/pacotes_nowo_controller.rb
+++ app/controllers/pacotes/pacotes_nowo_controller.rb
@@ -1,2 +1,28 @@
 class Pacotes::PacotesNowoController < ApplicationController
+  before_action :render_pacotes, except: %i[index]
+  
+  def index  
+  end
+  
+  #M4
+  def tv_net_voz
+  end
+
+  #M3
+  def telemovel
+  end
+  
+  def gaming
+  end
+  
+  def outros
+  end
+
+  private
+
+  def render_pacotes
+    respond_to do |format|
+      format.js { render partial: "pacotes/pacotes_nowo/#{params[:option]}" }
+    end 
+  end
 end
