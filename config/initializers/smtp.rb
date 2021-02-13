43a6cee57f2b0b2a494d118d2b500801927b21e7
samuel <samiequipo@gmail.com>
2021-02-13 17:14:05 -0400

    test 4

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -7,12 +7,3 @@
 #   authentication: :login,
 #   enable_starttls_auto: true
 # }
-
-ActionMailer::Base.smtp_settings = {
-  domain: 'ideallsite-2.herokuapp.com',
-  address: "smtp.sendgrid.net",
-  port: 587,
-  authentication: :plain,
-  user_name: 'apikey',
-  password: Rails.application.credentials.sendgrid[:api_key]
-}
