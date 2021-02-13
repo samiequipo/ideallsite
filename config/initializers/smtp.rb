ea3be3d5a53636ed5400dc363ee1ba359efcd28e
samuel <samiequipo@gmail.com>
2021-02-13 18:15:11 -0400

    test 7

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
   domain: 'heroku.com',
-  user_name: Rails.application.credentials.sendgrid[:USERNAME],
-  password: Rails.application.credentials.sendgrid[:PASSWORD],
-  authentication: :login,
-  enable_starttls_auto: true
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  user_name: 'apikey',
+  password: Rails.application.credentials.sendgrid[:api_key]
 }
