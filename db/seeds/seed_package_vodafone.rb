c62550f16e4fe80690b54ff505a6455bad3f568d
samuel <samiequipo@gmail.com>
2021-02-15 13:37:05 -0400

    second filled vodafone

--- db/seeds/seed_package_vodafone.rb
+++ db/seeds/seed_package_vodafone.rb
@@ -54,10 +54,50 @@ vodafone_telemovel_list = [
   ],
 ]
 
+# FIBRA 3 - TELEMOVEL
+# description, price, tv, voz, type
+vodafone_tv_net_voz_list = [
+  [ "Fibra 3 Play", "40,9", 
+    "TV - 140 Canais VBox 4K incluída | Amazon Prime Video y HBO Portugal 24 meses - FOX+ durante 24 meses", 
+    "Internet - 500/100 Mbps - Smart Router 2.0", 
+    "VOZ - Chamadas incluídas Para a rede fixa e 31 destinos internacionais",
+    Type.find_by(plano: "TV Net Voz", company: Company.find_by("description like ?", "%vodafone%"))
+
+  ],
+
+  [ "Fibra 3 Gold", "36,9", 
+    "TV - 140 Canais | Amazon Prime Video 6 meses - HBO Portugal 24 meses y FOX+ durante 24 meses", 
+    "Internet - 200/100 Mbps - Smart Router", 
+    "VOZ - Chamadas incluídas Para a rede fixa e 31 destinos internacionais",
+    Type.find_by(plano: "TV Net Voz", company: Company.find_by("description like ?", "%vodafone%"))
+
+  ],
+
+  [ "Fibra 3 Plus", "34,9", 
+    "TV - 140 Canais Box HD | HBO Portugal durante 3 meses", 
+    "Internet - 100/100 Mbps - Smart Router", 
+    "VOZ - Chamadas incluídas Para a rede fixa e 31 destinos internacionais",
+    Type.find_by(plano: "TV Net Voz", company: Company.find_by("description like ?", "%vodafone%"))
+
+  ],
+
+  [ "Fibra 3 Light", "30,9", 
+    "TV - 100 Canais | Não inclui Tv Box", 
+    "Internet - 30/30 Mbps - Smart Router", 
+    "VOZ - Chamadas incluídas Para a rede fixa e 31 destinos internacionais",
+    Type.find_by(plano: "TV Net Voz", company: Company.find_by("description like ?", "%vodafone%"))
+
+  ],
+]
+
 
 #Filling Packages
 vodafone_telemovel_list.each do |description, price, tv, internet, movel, cartao, voz, type|
   Package.create!(description: description, price: price, tv: tv, internet: internet, movel: movel, cartao: cartao, voz: voz, type: type)
+end
+
+vodafone_tv_net_voz_list.each do |description, price, tv, internet, voz, type|
+  Package.create!(description: description, price: price, tv: tv, internet: internet, voz: voz, type: type)
 end
 
 
