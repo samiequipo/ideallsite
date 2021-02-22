3fe3675d866a0598dc5696d0b10e239683dddda1
samuel <samiequipo@gmail.com>
2021-02-22 15:20:22 -0400

    whatsapp already list

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,10 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new(package_id: params[:package_id])
   end
+
+  def data
+    return "funciono"
+  end
   
   def create
     # byebug
@@ -13,7 +17,54 @@ class ContactsController < ApplicationController
       ContactMailer.with(contact: @contact).new_contact_email.deliver_later
       
       # Twilio Whatsapp
-      message = "The coffee roast '#{@contact.total_name}' was just added."
+      # User post data in meo || nos || vodafone || nowo path 
+      if @contact.package
+        message = "Ideall tem um novo cliente 
+
+        Name: #{@contact.total_name}
+        Email: #{@contact.email}
+        Phone: (+593) #{@contact.phone}
+        N° Cliente: #{@contact.visit_id}
+        ----------------------------------
+        País: #{@contact.visit.country}
+        Cidade: #{@contact.visit.city}
+        Região: #{@contact.visit.region}
+        ----------------------------------
+        Company: #{@contact.package.type.company.description}
+        Plano: #{@contact.package.type.plano}
+        ----------------------------------
+        #{@contact.package.description}
+        ---------
+        ° € #{@contact.package.price}/mês
+        ---
+        ° #{@contact.package.tv}
+        ---
+        ° #{@contact.package.internet}
+        ---
+        ° #{@contact.package.movel}
+        ---
+        ° #{@contact.package.cartao}
+        ---
+        ° #{@contact.package.voz}
+        ---
+        "
+      else
+        message = "Ideall tem um novo cliente 
+
+        Name: #{@contact.total_name}
+        Email: #{@contact.email}
+        Phone: (+593) #{@contact.phone}
+        N° Cliente: #{@contact.visit_id}
+        ----------------------------------
+        País: #{@contact.visit.country}
+        Cidade: #{@contact.visit.city}
+        Região: #{@contact.visit.region}
+        
+        "
+      end
+
+
+
       TwilioTextMessenger.new(message).call
 
       respond_to do |format|
