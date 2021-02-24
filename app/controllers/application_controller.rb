e9f8fce852fe9d968562ff6a8bb09265282c8ca9
samuel <samiequipo@gmail.com>
2021-02-24 17:25:39 -0400

    fix current_visit last visit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :current_visit
   helper_method :current_visit
 
-  def current_visit
-    @current_visit ||= Ahoy::Visit.last
-  end
+  private 
+    def current_visit
+      @current_visit ||= Ahoy::Visit.last
+    end
 end
