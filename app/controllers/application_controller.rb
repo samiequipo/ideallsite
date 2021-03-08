9f0df0a91096fd8c5842d735c2146a0d2ede841d
samuel <samiequipo@gmail.com>
2021-03-08 10:03:02 -0400

    fix ahoy event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   # Call and get visit
   before_action :current_visit
+  before_action :current_event
   helper_method :current_visit
 
   private 
     def current_visit
       @current_visit ||= Ahoy::Visit.last
+    end
+    
+    def current_event
+      Ahoy::Event.create visit: Ahoy::Visit.last if (Ahoy::Event.count == 0)
     end
 end
