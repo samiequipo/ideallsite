24a281d2d3520c3635f3ba9c5e03e83b71327011
samuel <samiequipo@gmail.com>
2021-03-09 20:45:17 -0400

    change ip cp

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '178.62.33.125', user: 'deploy', roles: %w{app db web}
+server '46.101.44.27', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
