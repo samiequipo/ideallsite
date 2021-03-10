866c3b32614c335012c81d3ad1d9c9e5fb71bc27
samuel <samiequipo@gmail.com>
2021-03-10 19:18:04 -0400

    revert

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module PortfolioEpisode1
        html_tag
     }
 
-    configure.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
