2afece9b6f4f9150c6a7e89554489c145ea3df5d
samuel <samiequipo@gmail.com>
2021-03-25 13:23:38 -0400

    remove glider.js and add carousel

--- public/startbootstrap-agency-gh-pages/js/about.js
+++ public/startbootstrap-agency-gh-pages/js/about.js
@@ -145,24 +145,3 @@ gsap.from(".icons span", {
   x: -30,
   stagger: 0.2,
 });
-
-const glide = document.querySelector(".glide");
-if (glide)
-  new Glide(glide, {
-    type: "carousel",
-    startAt: 0,
-    perView: 3,
-    gap: 30,
-    hoverpause: true,
-    autoplay: 2000,
-    animationDuration: 800,
-    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
-    breakpoints: {
-      996: {
-        perView: 2,
-      },
-      768: {
-        perView: 1,
-      },
-    },
-  }).mount();
