a1d3d2886bfc32645dad67115513a53eaec5476e
samuel <samiequipo@gmail.com>
2021-04-07 10:10:35 -0400

    With luis correo

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def new_contact_email
     @contact = params[:contact]
 
-    mail(to: ['samiequipo@gmail.com'],
+    mail(to: ['luiscastro@ideall.pt', 'samiequipo@gmail.com'],
          subject: "You got a new contact!")
   end
 end
